d0fdf93d77aed0f25d66c6ae8e261b360b21bc08
Nnncyyy <delcastillo.aguirre.nancy5@gmail.com>
2025-02-23 22:31:31 -0600

    se añadio la funcion guardar contactos con un Archivo

--- main.c
+++ main.c
@@ -4,19 +4,18 @@
 struct agenda
 {
     char nombre[50];
-    char num_movil[15];
+    char telefono[15];
     char email[50];
     int id;
 };
 
-//Prototipos de funciones
-int buscarID(char[30], struct agenda contacto[100]);
-void eliminarContacto(int, struct agenda contacto[100]);
+//Prototipos de funciones.
+int guardarContacto(int num_contacto, struct agenda contacto[100]);
 
 int main() {
     struct agenda contacto[100];
-    int op, i, j, k, num_contacto=0, buscar_id, id_encontrado;
-    char buscar_nombre[30];
+    int op, num_contacto=0, id_encontrado;
+    char buscar_nombre[50];
 
     printf("Agenda Digital de 100 contactos. \n");
 
@@ -26,31 +25,26 @@ int main() {
         //printf("1. Agregar contacto  \n");
         //printf("2. Mostrar contacto  \n");
         //printf("3. Buscar contacto   \n");
-        printf("4. Eliminar contacto \n");
+        //printf("4. Eliminar contacto \n");
+        printf("5. Guardar contacto  \n");
         printf("0. Salir \n");
         printf("Seleccione una opcion\n");
         scanf("%d",&op);
+        fflush(stdin);
 
         switch (op)
         {
-
-        case 4:
-            __fpurge(stdin);
-            printf("5. Eliminar contacto. \n");
-            printf("Ingrese el nombre del contacto que desea eliminar: \n");
-            scanf("%[^\n]",buscar_nombre);
-            __fpurge(stdin);
-
-            id_encontrado = buscarID(buscar_nombre,contacto);
-    
-            if (id_encontrado != 200){
-                eliminarContacto(id_encontrado, contacto);
-                printf("Contacto elimanado con exito. \n");
-            } else if (id_encontrado == 200) {
-                printf("Contacto NO encontrado. \n");
+        //casos anteriores
+        case 5:
+            printf("5. Guardar contacto  \n");
+            if(num_contacto != 0){
+                guardarContacto(num_contacto,contacto);
+                printf("Contactos guardados con exito. \n");
+            } else {
+                printf("No se a registrado ningun contacto. \n");
             }
-            break;
-        
+            break;    
+            
         case 0:
             printf("Saliendo.... \n");
             break;
@@ -61,40 +55,27 @@ int main() {
 
     } while (op != 0);
     
-
     return 0;
-
 }
 
 // Definición de funciones.
 
-// Función para buscar la ID de un contacto.
-int buscarID(char buscar_nombre[30], struct agenda contacto[100]){
-    int i=0, j, k=0, buscar_id;
-    do{
-        for (j = 0; buscar_nombre[j] != '\0'; j++){
-            if (contacto[i].nombre[j] == buscar_nombre[j]){
-                buscar_id = i;
-            } else {
-                i++;
-            }
-            k++;
+int guardarContacto(int num_contacto, struct agenda contacto[100]){
+    FILE *archivo;
+    archivo = fopen("contactos.txt", "w");
+            
+    if (archivo == NULL){
+        printf("Error al abrir el archivo para escritura.\n");
+        return 1;
         }
-    } while (k <= 100);
+             
+    for(int i=0; i<num_contacto; i++){
+        fprintf(archivo, "------------ Contacto No.%d ------------\n", contacto[i].id+1);
+        fprintf(archivo, "Nombre: %s\n", contacto[i].nombre);
+        fprintf(archivo, "Telefono: %s\n", contacto[i].telefono);
+        fprintf(archivo, "Email: %s\n", contacto[i].email);
+        fprintf(archivo, "\n");
+        }
 
-    if (i >= 100){
-        buscar_id = 200;
-        return buscar_id;
-    } else {
-        return buscar_id;
-    }
-}
-
-// Función para "eliminar" un contacto.
-void eliminarContacto(int id_encontrado, struct agenda contacto[100]){
-    contacto[id_encontrado].nombre[0] = ' ';
-    contacto[id_encontrado].nombre[1] = ' ';
-    contacto[id_encontrado].num_movil[15] = ' ';
-    contacto[id_encontrado].email[50] = ' ';
-    contacto[id_encontrado].id = 300;
+        fclose(archivo);
 }
